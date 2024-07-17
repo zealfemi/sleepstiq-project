fd6f7d8a6310eebf1680463d657be1d212f90881
zealfemi <zealfemi@gmail.com>
2024-07-17 22:52:10 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <img
+          src="./images/banner-home.png"
+          alt="header logo"
+          className="w-100 h-100 banner-img"
+        />
+
+        <div className="container">
+          <div className="header-contents">
+            <nav className="header-nav">
+              <img
+                src="./images/logos/sleepstiq-header.png"
+                alt="header logo"
+                className="header-logo"
+              />
+
+              <ul className="header-nav-links">
+                <li>
+                  <a href="./#" className="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="./#">About</a>
+                </li>
+                <li>
+                  <a href="./#">Shop</a>
+                </li>
+                <li>
+                  <a href="./#">FAQs</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="banner-contents">
+              <p className="banner-top-text">We're here to help you</p>
+              <h1 className="banner-large-text">Relax &amp; Rest</h1>
+              <p className="banner-bottom-text">
+                With the aid of our Melatonin Sleepstiq, we can assure you that
+                you can get quality sleep.
+              </p>
+            </div>
+            <button className="btn">Visit Shop</button>
+          </div>
+        </div>
+
+        <div className="header-partners-container">
+          <div className="header-partners">
+            <img src="./images/logos/google.png" className="" alt="partners" />
+            <img src="./images/logos/google.png" className="" alt="partners" />
+            <img src="./images/logos/google.png" className="" alt="partners" />
+            <img src="./images/logos/google.png" className="" alt="partners" />
+            <img src="./images/logos/google.png" className="" alt="partners" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
