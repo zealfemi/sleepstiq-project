ab047c0c707b2e1f047d1ab82c01fabe86b3a77c
zealfemi <zealfemi@gmail.com>
2024-07-18 05:07:27 +0100

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -88,6 +88,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
+          <p className="text-center">Designed by Mackie</p>
         </div>
       </footer>
     </>
