f363f4b5154a2594d2b0815bdcd8f53ac42253df
zealfemi <zealfemi@gmail.com>
2024-07-19 15:56:54 +0100

    first commit

--- src/components/FAQsAccordion.js
+++ src/components/FAQsAccordion.js
@@ -10,14 +10,26 @@ import data from "../data/faqs";
 export default function FAQsAccordion(props) {
   const catId = props.categoryId;
 
+  const [expanded, setExpanded] = useState(false);
+
+  function handleChange(isExpanded, panel) {
+    setExpanded(isExpanded ? panel : false);
+  }
+
   const accordion = data[catId].data?.map((accordionList) => {
     return (
       <Accordion
-        id="panel1-header"
-        aria-controls="panel1-content"
-        key={accordionList.id}
+        expanded={expanded === accordionList.id}
+        onChange={(event, isExpanded) =>
+          handleChange(isExpanded, accordionList.id)
+        }
       >
-        <AccordionSummary>{accordionList.question}</AccordionSummary>
+        <AccordionSummary
+          id={accordionList.id}
+          aria-controls={accordionList.id}
+        >
+          {accordionList.question}
+        </AccordionSummary>
         <AccordionDetails>{accordionList.answer}</AccordionDetails>
       </Accordion>
     );
